a4ddc829f82b8d9167dc94a0c81b37086e89a05d
Franco <furankohana@gmail.com>
2022-06-12 17:41:59 +0900

    we made it a server

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-var mensaje = "Esto se ve mas apropiado";
-console.log(mensaje);
+const http = require("http")
+const port = 3000
+
+const server = http.createServer(function(req, res){
+    res.write("Hola Mi conejillo de indias")
+    res.end()
+}) 
+
+server.listen(port, function(error){
+    if (error) {
+        console.log(" No funciono :s", error)
+    } else {
+        console.log(" Basicamente funciono " + port)
+    }
+})
