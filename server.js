741b410b109ad9f4e94ee44f07778d165ca60b44
Jose Andres Flores <jandresfloress@gmail.com>
2023-02-07 07:56:17 +0900

    In the middle of implementation

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Importing express module
 var express = require("express");
+const mysql = require('mysql')
+
  
 // Importing mongoose module
-var mongoose = require("mongoose");
 const port = 3000;
 const app = express();
 
@@ -17,6 +18,20 @@ app.listen(port, () => {
   console.log(`The application started
   successfully on port ${port}`);
 });
+
+//Get game form
+app.get("/gameform", (req, res) => {
+  return res.render("gameform.ejs")
+});
+
+//Add game
+app.post("/gameform", (req, res) => {
+  let game = new Game(req.body.gameName, req.body.categories, req.body.multiplayer)
+  addGame(game);
+  return res.render("gameform.ejs", {game: game});
+});
+
+
 
 // default URL for website
 app.get('/', (req,res) => {
@@ -38,3 +53,44 @@ app.post("/enviarForm", (req, res) => {
 });
 
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: "3306",
+  user: 'root',
+  password: 'mysql',
+  database: 'francoweb'
+})
+
+function connectToDB() {
+    connection.connect();
+    console.log("Estamos conectados");
+}
+
+function disconnect(){
+    connection.end()
+}
+
+/**
+ * 
+ * @param {Game} game 
+ */
+function addGame(game){
+    connectToDB();
+    let queryStr = 'INSERT INTO francoweb.GAMES(name, temp_category, multiplayer) VALUES("' + game.name +'","' + game.category + '", "' + game.multiplayer + '")';
+    console.log("query > " + queryStr);
+    connection.query(queryStr, (err, rows) => {
+        if (err) throw err
+      
+        console.log('The solution is: ', rows[0].solution)
+      });
+      console.log("Ok eso paso? " + JSON.stringify(game));
+      disconnect();
+}
+
+class Game {
+    constructor(name, category, multiplayer){
+        this.name = name;
+        this.category = category;
+        this.multiplayer = multiplayer;
+    }
+}
